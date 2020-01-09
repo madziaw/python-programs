bc32ee443e09f278b66c6c8f5ec0873814eab2bd
Magdalena Wasilewska <magdalena.barbara.wasilewska@gmail.com>
2020-01-10 00:04:08 +0100

    Add very simple text editor

---
+++ file.py
@@ -0,0 +1,14 @@
+choice = input('Enter 1 to read a file, Enter 2 to write to a file: ')
+if choice == '1':
+    file_name = input('Enter file name: ')
+    file_name = open(file_name,'r')    
+    a = file_name.read()
+    print(a) 
+elif choice == '2':
+    file_name = input('Enter file name: ')
+    file_name = open(file_name, 'w')
+    text = input('Enter text: ')   
+    file_name.write(text +'\n')
+    file_name.close()
+else:
+    print('Error. Choose 1 or 2.')
